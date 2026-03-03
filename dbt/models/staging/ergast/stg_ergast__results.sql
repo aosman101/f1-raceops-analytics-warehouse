d1eb88aecc6936be3ef98bb7c66d854d24bad694
Adil <aosman15@student.bbk.ac.uk>
2026-03-03 18:27:13 +0000

    Refactor SQL queries in stg_ergast__results.sql and stg_ergast__status.sql for improved clarity and structure

--- dbt/models/staging/ergast/stg_ergast__results.sql
+++ dbt/models/staging/ergast/stg_ergast__results.sql
@@ -1,4 +1,17 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ source('ergast', 'results') }}
+select
+  result_id::int as result_id,
+  race_id::int as race_id,
+  driver_id::int as driver_id,
+  constructor_id::int as constructor_id,
+  nullif(number, '')::int as car_number,
+  grid::int as grid_position,
+  nullif(position, '')::int as classified_position,
+  position_order::int as position_order,
+  points::numeric as points,
+  laps::int as laps,
+  nullif(milliseconds, '')::bigint as finish_ms,
+  nullif(fastest_lap, '')::int as fastest_lap,
+  nullif(rank, '')::int as fastest_lap_rank,
+  nullif(fastest_lap_speed, '')::numeric as fastest_lap_speed,
+  status_id::int as status_id
+from {{ source('ergast', 'results') }};
--- dbt/models/staging/ergast/stg_ergast__status.sql
+++ dbt/models/staging/ergast/stg_ergast__status.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ source('ergast', 'status') }}
+select
+  status_id::int as status_id,
+  status
+from {{ source('ergast', 'status') }};
