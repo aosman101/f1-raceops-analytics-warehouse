533cfbdd264c6f3e4f7240ec5e57813d8079ae60
Adil <aosman15@student.bbk.ac.uk>
2026-03-03 18:26:23 +0000

    Refactor SQL queries in stg_ergast__circuits.sql and stg_ergast__races.sql for improved clarity and structure

--- dbt/models/staging/ergast/stg_ergast__circuits.sql
+++ dbt/models/staging/ergast/stg_ergast__circuits.sql
@@ -1,4 +1,11 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ source('ergast', 'circuits') }}
+select
+  circuit_id::int as circuit_id,
+  circuit_ref,
+  name as circuit_name,
+  location,
+  country,
+  nullif(lat, '')::numeric as lat,
+  nullif(lng, '')::numeric as lng,
+  nullif(alt, '')::int as alt,
+  url
+from {{ source('ergast', 'circuits') }};
--- dbt/models/staging/ergast/stg_ergast__races.sql
+++ dbt/models/staging/ergast/stg_ergast__races.sql
@@ -1,4 +1,10 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ source('ergast', 'races') }}
+select
+  race_id::int as race_id,
+  year::int as season_year,
+  round::int as round,
+  circuit_id::int as circuit_id,
+  name as race_name,
+  date::date as race_date,
+  nullif(replace(time, 'Z', ''), '')::time as race_time,
+  url
+from {{ source('ergast', 'races') }};
