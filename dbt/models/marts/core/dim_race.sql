0e9b78fd459a0aa7ecd57c0e6caea8c1da23e5e6
Adil <aosman15@student.bbk.ac.uk>
2026-03-04 17:30:48 +0000

    Refactor SQL queries in dim_race.sql and fct_race_results.sql for improved clarity and structure

--- dbt/models/marts/core/dim_race.sql
+++ dbt/models/marts/core/dim_race.sql
@@ -1,4 +1,13 @@
-{{ config(materialized='table') }}
-
-select *
-from {{ ref('stg_ergast__races') }}
+select
+  r.race_id,
+  r.season_year,
+  r.round,
+  r.circuit_id,
+  c.circuit_name,
+  c.country,
+  r.race_name,
+  r.race_date,
+  r.race_time
+from {{ ref('stg_ergast__races') }} r
+left join {{ ref('stg_ergast__circuits') }} c
+  on r.circuit_id = c.circuit_id;
--- dbt/models/marts/core/fct_race_results.sql
+++ dbt/models/marts/core/fct_race_results.sql
@@ -1,4 +1,38 @@
-{{ config(materialized='table') }}
+select
+  r.race_id,
+  r.season_year,
+  r.round,
+  r.race_date,
 
-select *
-from {{ ref('stg_ergast__results') }}
+  res.driver_id,
+  res.constructor_id,
+  res.grid_position,
+  res.position_order,
+  res.classified_position,
+  res.points,
+  res.laps,
+  res.finish_ms,
+  res.status_id,
+  s.status,
+
+  case when res.position_order = 1 then 1 else 0 end as is_win,
+  case when res.position_order <= 3 then 1 else 0 end as is_podium,
+
+  -- "Finished" and "+N Laps" are classified finishes; everything else treat as DNF/Not Classified
+  case
+    when s.status = 'Finished' then 1
+    when s.status like '+% Lap%' then 1
+    else 0
+  end as is_classified_finish,
+
+  case
+    when s.status = 'Finished' then 0
+    when s.status like '+% Lap%' then 0
+    else 1
+  end as is_dnf
+
+from {{ ref('stg_ergast__results') }} res
+join {{ ref('stg_ergast__races') }} r
+  on res.race_id = r.race_id
+left join {{ ref('stg_ergast__status') }} s
+  on res.status_id = s.status_id;
