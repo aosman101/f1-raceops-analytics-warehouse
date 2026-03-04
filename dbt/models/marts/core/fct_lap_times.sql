d0a41c42ce0edc0467bea4683f25365b01015fc8
Adil <aosman15@student.bbk.ac.uk>
2026-03-04 17:32:47 +0000

    Refactor SQL queries in fct_lap_times.sql and fct_qualifying.sql for improved clarity and structure

--- dbt/models/marts/core/fct_lap_times.sql
+++ dbt/models/marts/core/fct_lap_times.sql
@@ -1,4 +1,13 @@
-{{ config(materialized='table') }}
-
-select *
-from {{ ref('stg_ergast__lap_times') }}
+select
+  lt.race_id,
+  r.season_year,
+  lt.driver_id,
+  rr.constructor_id,
+  lt.lap,
+  lt.position,
+  lt.lap_ms
+from {{ ref('stg_ergast__lap_times') }} lt
+join {{ ref('stg_ergast__races') }} r
+  on lt.race_id = r.race_id
+left join {{ ref('stg_ergast__results') }} rr
+  on lt.race_id = rr.race_id and lt.driver_id = rr.driver_id;
--- dbt/models/marts/core/fct_qualifying.sql
+++ dbt/models/marts/core/fct_qualifying.sql
@@ -1,4 +1,12 @@
-{{ config(materialized='table') }}
-
-select *
-from {{ ref('stg_ergast__qualifying') }}
+select
+  q.race_id,
+  r.season_year,
+  q.driver_id,
+  q.constructor_id,
+  q.qualifying_position,
+  q.q1_ms,
+  q.q2_ms,
+  q.q3_ms
+from {{ ref('stg_ergast__qualifying') }} q
+join {{ ref('stg_ergast__races') }} r
+  on q.race_id = r.race_id;
