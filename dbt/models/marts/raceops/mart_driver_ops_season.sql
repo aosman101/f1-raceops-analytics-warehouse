778c7e708daac634a27ec2fb15a7e8aa502bdf80
Adil <aosman15@student.bbk.ac.uk>
2026-03-04 17:35:54 +0000

    Refactor SQL queries in mart_constructor_ops_season.sql and mart_driver_ops_season.sql for improved clarity and structure

--- dbt/models/marts/raceops/mart_driver_ops_season.sql
+++ dbt/models/marts/raceops/mart_driver_ops_season.sql
@@ -1,5 +1,33 @@
-{{ config(materialized='table') }}
+with res as (
+  select * from {{ ref('fct_race_results') }}
+),
+pit as (
+  select
+    season_year,
+    driver_id,
+    count(*) as pit_stops,
+    avg(pit_ms) as avg_pit_ms
+  from {{ ref('fct_pit_stops') }}
+  where pit_ms is not null
+  group by 1,2
+)
+select
+  res.season_year,
+  res.driver_id,
 
-select *
-from {{ ref('fct_race_results') }}
-limit 0
+  sum(res.points) as points,
+  sum(res.is_win) as wins,
+  sum(res.is_podium) as podiums,
+  sum(res.is_dnf) as dnfs,
+
+  avg(res.grid_position)::numeric(10,2) as avg_grid,
+  avg(res.position_order)::numeric(10,2) as avg_finish,
+
+  coalesce(pit.pit_stops, 0) as pit_stops,
+  coalesce(pit.avg_pit_ms, null) as avg_pit_ms
+
+from res
+left join pit
+  on res.season_year = pit.season_year
+ and res.driver_id = pit.driver_id
+group by 1,2, pit.pit_stops, pit.avg_pit_ms;
