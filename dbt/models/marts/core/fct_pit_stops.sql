74425979e872c58d3d70ee7994f364a731f4fcde
Adil <aosman15@student.bbk.ac.uk>
2026-03-04 17:31:27 +0000

    Refactor SQL query in fct_pit_stops.sql for improved clarity and structure

--- dbt/models/marts/core/fct_pit_stops.sql
+++ dbt/models/marts/core/fct_pit_stops.sql
@@ -1,4 +1,13 @@
-{{ config(materialized='table') }}
-
-select *
-from {{ ref('stg_ergast__pit_stops') }}
+select
+  p.race_id,
+  r.season_year,
+  p.driver_id,
+  rr.constructor_id,
+  p.stop_number,
+  p.lap,
+  p.pit_ms
+from {{ ref('stg_ergast__pit_stops') }} p
+join {{ ref('stg_ergast__races') }} r
+  on p.race_id = r.race_id
+left join {{ ref('stg_ergast__results') }} rr
+  on p.race_id = rr.race_id and p.driver_id = rr.driver_id;
